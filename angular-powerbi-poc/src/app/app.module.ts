dbcdc244b89ec6a8dd4a3aaf66be4fbfe78c5634
Atanu Sarkar <mratanusarkar@gmail.com>
2022-05-25 09:27:50 +0530

    add package powerbi-client-angular

--- angular-powerbi-poc/src/app/app.module.ts
+++ angular-powerbi-poc/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PowerBIEmbedModule } from 'powerbi-client-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PowerBIEmbedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
