43bee76350de8648275526ef9c2f22ed5c275cc8
Atanu Sarkar <mratanusarkar@gmail.com>
2022-05-25 10:31:37 +0530

    added HttpClientModule

--- angular-powerbi-poc/src/app/app.module.ts
+++ angular-powerbi-poc/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { PowerBIEmbedModule } from 'powerbi-client-angular';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     PowerBIEmbedModule
   ],
   providers: [],
